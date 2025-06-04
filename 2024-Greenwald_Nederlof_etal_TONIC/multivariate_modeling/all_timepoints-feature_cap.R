e28c11607e364f7b1a959ce4f0cb1118b6a655ed
csowers <camisowers@g.ucla.edu>
2025-06-04 12:26:12 -0700

    tonic rna path and ED 15h updates

---
+++ 2024-Greenwald_Nederlof_etal_TONIC/multivariate_modeling/all_timepoints-feature_cap.R
@@ -0,0 +1,210 @@
+library(glmnet)
+library(caret)
+library(xgboost)
+library(pROC)
+library(ggplot2)
+library(ggsignif)
+library(ggpubr)
+library(dplyr)
+library(MASS)
+library(splitTools)
+library(multiview)
+
+source("utils.R")
+
+
+max_feats = 20
+max_feats = max_feats - 1
+
+######################### ######################### ######################### 
+######################### On Nivo #########################
+######################### ######################### #########################
+
+df_dir = paste('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/prediction_model', sep = '')
+out_dir = paste('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/supplementary_figs/review_figures/limit_model_features/prediction_model_', max_feats+1, '/patient_outcomes', sep = '')
+
+
+df_X_matrix = read.csv(paste(df_dir, '/df_matrix_on_nivo.csv', sep = ''), header=TRUE)
+colnames(df_X_matrix)[1] = "Patient_ID"
+
+df = read.csv(paste(df_dir, '/timepoint_combined_features.csv', sep = ''), header=TRUE)
+df_label = df[,c("Patient_ID", "Clinical_benefit")]
+aggregated_df = df_label %>%
+  distinct()
+seed_seq = seq(51,60)
+
+merge_df = merge(df_X_matrix, aggregated_df, by="Patient_ID")
+merged_df_order = merge_df[, c(names(df_label), setdiff(names(merge_df), names(df_label)))]
+
+feature_matrix = merged_df_order[,3:dim(merged_df_order)[2]]
+response_label_raw = merged_df_order[,2]
+response_label = response_label_raw
+response_label_on_nivo = as.numeric(response_label == "Yes")
+
+feature_on_nivo = as.matrix(feature_matrix)
+feature_on_nivo[is.na(feature_on_nivo)] = 0
+
+nfolds = 3
+auc_on_nivo_list = c()
+feature_selected_on_nivo = list()
+i = 1
+for (seed_i in seed_seq){
+  set.seed(seed_i)
+  folds =  stratified_k_fold_indices(y = response_label_on_nivo, k = nfolds)
+  
+  fit_on_nivo = cv.glmnet(feature_on_nivo, 
+                          response_label_on_nivo, 
+                          family='binomial', 
+                          type.measure='auc', 
+                          foldid = folds,
+                          standardize=FALSE, 
+                          dfmax=max_feats)
+  
+  auc_on_nivo = max(fit_on_nivo$cvm) 
+  auc_on_nivo_list = c(auc_on_nivo_list, auc_on_nivo)
+  
+  feature_selected_on_nivo[[i]] = get_ranked_feature(fit_on_nivo, feature_on_nivo)[1:20,]
+  i = i + 1
+}
+
+mean(auc_on_nivo_list)
+combined_results <- do.call(cbind, feature_selected_on_nivo)
+write.csv(combined_results, file = paste(out_dir, "/top_features_results_on_nivo_MIBI.csv", sep = ''), row.names = FALSE)
+#write.csv(auc_on_nivo_list, file = "./results_100_runs/results_on_nivo_MIBI_100_runs_previous.csv", row.names = FALSE)
+
+######################### ######################### ######################### 
+######################### Baseline #########################
+######################### ######################### ######################### 
+
+df_X_matrix = read.csv(paste(df_dir, '/df_matrix_baseline.csv', sep = ''), header=TRUE)
+colnames(df_X_matrix)[1] = "Patient_ID"
+
+merge_df = merge(df_X_matrix, aggregated_df, by="Patient_ID")
+merged_df_order = merge_df[, c(names(df_label), setdiff(names(merge_df), names(df_label)))]
+
+feature_matrix = merged_df_order[,3:dim(merged_df_order)[2]]
+response_label_raw = merged_df_order[,2]
+response_label = response_label_raw
+response_label_baseline = as.numeric(response_label == "Yes")
+
+feature_baseline = as.matrix(feature_matrix)
+feature_baseline[is.na(feature_baseline)] = 0
+
+nfolds = 3
+auc_baseline_list = c()
+feature_selected_baseline = list()
+i = 1
+
+for (seed_i in seed_seq){
+  set.seed(seed_i)
+  folds =  stratified_k_fold_indices(y = response_label_baseline, k = nfolds)
+  
+  fit_baseline = cv.glmnet(feature_baseline, 
+                           response_label_baseline, 
+                           family='binomial', 
+                           type.measure='auc', 
+                           foldid = folds,
+                           standardize=FALSE, 
+                           dfmax=max_feats)
+  
+  auc_baseline = max(fit_baseline$cvm) 
+  auc_baseline_list = c(auc_baseline_list, auc_baseline)
+  
+  feature_selected_baseline[[i]] = get_ranked_feature(fit_baseline, feature_baseline)[1:20,]
+  i = i + 1
+}
+
+combined_results <- do.call(cbind, feature_selected_baseline)
+write.csv(combined_results, file = paste(out_dir, "/top_features_results_baseline_MIBI.csv", sep=''), row.names = FALSE)
+
+
+######################### ######################### ######################### 
+######################### Post Induction #########################
+######################### ######################### ######################### 
+
+df_X_matrix = read.csv(paste(df_dir, '/df_matrix_pre_nivo.csv', sep=''), header=TRUE)
+colnames(df_X_matrix)[1] = "Patient_ID"
+
+merge_df = merge(df_X_matrix, aggregated_df, by="Patient_ID")
+merged_df_order = merge_df[, c(names(df_label), setdiff(names(merge_df), names(df_label)))]
+
+feature_matrix = merged_df_order[,3:dim(merged_df_order)[2]]
+response_label_raw = merged_df_order[,2]
+response_label = response_label_raw
+response_label_post_induction = as.numeric(response_label == "Yes")
+
+feature_post_induction = as.matrix(feature_matrix)
+feature_post_induction[is.na(feature_post_induction)] = 0
+
+auc_post_induction_list = c()
+nfolds = 3
+feature_selected_post_induction = list()
+i = 1
+
+for (seed_i in seed_seq){
+  set.seed(seed_i)
+  folds = stratified_k_fold_indices(y = response_label_post_induction, k = nfolds)
+  
+  fit_post_induction = cv.glmnet(feature_post_induction, 
+                                 response_label_post_induction, 
+                                 family='binomial', 
+                                 type.measure='auc', 
+                                 foldid = folds,
+                                 standardize=FALSE, 
+                                 dfmax=max_feats)
+  
+  auc_post_induction = max(fit_post_induction$cvm)
+  auc_post_induction_list = c(auc_post_induction_list, auc_post_induction)
+  feature_selected_post_induction[[i]] = get_ranked_feature(fit_post_induction, feature_post_induction)[1:20,]
+  i = i + 1
+}
+
+combined_results <- do.call(cbind, feature_selected_post_induction)
+write.csv(combined_results, file = paste(out_dir, "/top_features_results_pre_nivo_MIBI.csv", sep=''), row.names = FALSE)
+
+######################### ######################### ######################### 
+######################### Primary #########################
+######################### ######################### ######################### 
+
+df_X_matrix = read.csv(paste(df_dir, '/df_matrix_primary.csv', sep=''), header=TRUE)
+colnames(df_X_matrix)[1] = "Patient_ID"
+
+merge_df = merge(df_X_matrix, aggregated_df, by="Patient_ID")
+merged_df_order = merge_df[, c(names(df_label), setdiff(names(merge_df), names(df_label)))]
+
+feature_matrix = merged_df_order[,3:dim(merged_df_order)[2]]
+response_label_raw = merged_df_order[,2]
+response_label = response_label_raw
+response_label_primary = as.numeric(response_label == "Yes")
+
+feature_primary = as.matrix(feature_matrix)
+feature_primary[is.na(feature_primary)] = 0
+
+nfolds = 3
+auc_primary_list = c()
+feature_selected_primary = list()
+i = 1
+
+for (seed_i in seed_seq){
+  set.seed(seed_i)
+  folds = stratified_k_fold_indices(y = response_label_primary, k = nfolds)
+  fit_primary = cv.glmnet(feature_primary,
+                          response_label_primary,
+                          family='binomial',
+                          type.measure='auc',
+                          foldid = folds,
+                          standardize=FALSE, 
+                          dfmax=max_feats)
+  auc_primary = max(fit_primary$cvm) 
+  auc_primary_list = c(auc_primary_list, auc_primary)
+  feature_selected_primary[[i]] = get_ranked_feature(fit_primary, feature_primary)[1:20,]
+  i = i + 1
+}
+
+combined_results <- do.call(cbind, feature_selected_primary)
+write.csv(combined_results, file = paste(out_dir, "/top_features_results_primary_MIBI.csv", sep=''), row.names = FALSE)
+
+
+# Save all timepoint resutls in csv 
+all_results = cbind(auc_on_nivo_list, auc_baseline_list, auc_post_induction_list, auc_primary_list)
+write.csv(all_results, file = paste(out_dir, "/all_timepoints_results_MIBI.csv", sep=''), row.names = FALSE)
--- 2024-Greenwald_Nederlof_etal_TONIC/multivariate_modeling/all_timepoints_rna_signature_final.R
+++ 2024-Greenwald_Nederlof_etal_TONIC/multivariate_modeling/all_timepoints_rna_signature_final.R
@@ -10,17 +10,16 @@ library(MASS)
 library(splitTools)
 library(multiview)
 
-source("utils_rna.R")
 
-######################### ############################# 
+######################### #############################
 ######################### Baseline ####################
 ######################### RNA #########################
 ######################### #############################
 
-df_X_matrix = read.csv('data/genomics/rna_df_matrix_baseline_signature.csv', header=TRUE)
+df_X_matrix = read.csv('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/sequencing_data/prediction/multivariate_lasso/rna_df_matrix_baseline_signature.csv', header=TRUE)
 colnames(df_X_matrix)[1] = "Patient_ID"
 
-df = read.csv('data/genomics/processed_genomics_features_final.csv', header=TRUE)
+df = read.csv('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/sequencing_data/prediction/processed_genomics_features.csv', header=TRUE)
 df_label = df[,c("Patient_ID", "Clinical_benefit")]
 aggregated_df = df_label %>%
   distinct()
@@ -50,33 +49,33 @@ i = 1
 for (seed_i in seed_seq){
   set.seed(seed_i)
   folds =  stratified_k_fold_indices(y = response_label_baseline_rna, k = nfolds)
-  
-  fit_baseline = cv.glmnet(feature_baseline_rna, #feature_baseline, 
-                           response_label_baseline_rna, 
-                          family='binomial', 
-                          type.measure='auc', 
+
+  fit_baseline = cv.glmnet(feature_baseline_rna, #feature_baseline,
+                           response_label_baseline_rna,
+                          family='binomial',
+                          type.measure='auc',
                           foldid = folds,
                           standardize=FALSE)
-  
-  auc_baseline = max(fit_baseline$cvm) 
+
+  auc_baseline = max(fit_baseline$cvm)
   auc_baseline_list_rna = c(auc_baseline_list_rna, auc_baseline)
-  
+
   feature_selected_baseline_rna[[i]] = get_ranked_feature(fit_baseline, feature_baseline_rna)[1:20,]
   i = i + 1
 }
 
 combined_results <- do.call(cbind, feature_selected_baseline_rna)
-write.csv(combined_results, file = "./results/genomics/top_features_results_baseline_RNA_signature.csv", row.names = FALSE)
+write.csv(combined_results, file = "/Volumes/Shared/Noah Greenwald/TONIC_Cohort/sequencing_data/prediction/top_features_results_baseline_RNA_signature.csv", row.names = FALSE)
 
-######################### ############################# 
+######################### #############################
 ######################### Induction ####################
 ######################### RNA #########################
 ######################### #############################
 
-df_X_matrix = read.csv('data/genomics/rna_df_matrix_post_induction_signature.csv', header=TRUE)
+df_X_matrix = read.csv('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/sequencing_data/prediction/multivariate_lasso/rna_df_matrix_post_induction_signature.csv', header=TRUE)
 colnames(df_X_matrix)[1] = "Patient_ID"
 
-df = read.csv('data/genomics/processed_genomics_features_final.csv', header=TRUE)
+df = read.csv('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/sequencing_data/prediction/processed_genomics_features.csv', header=TRUE)
 df_label = df[,c("Patient_ID", "Clinical_benefit")]
 aggregated_df = df_label %>%
   distinct()
@@ -105,33 +104,33 @@ i = 1
 for (seed_i in seed_seq){
   set.seed(seed_i)
   folds =  stratified_k_fold_indices(y = response_label_induction_rna, k = nfolds)
-  
-  fit_baseline = cv.glmnet(feature_induction_rna, #feature_baseline, 
-                           response_label_induction_rna, 
-                           family='binomial', 
-                           type.measure='auc', 
+
+  fit_baseline = cv.glmnet(feature_induction_rna, #feature_baseline,
+                           response_label_induction_rna,
+                           family='binomial',
+                           type.measure='auc',
                            foldid = folds,
                            standardize=FALSE)
-  
-  auc_baseline = max(fit_baseline$cvm) 
+
+  auc_baseline = max(fit_baseline$cvm)
   auc_baseline_list_rna_induction = c(auc_baseline_list_rna_induction, auc_baseline)
-  
+
   feature_selected_induction_rna[[i]] = get_ranked_feature(fit_baseline, feature_induction_rna)[1:20,]
   i = i + 1
 }
 
 combined_results <- do.call(cbind, feature_selected_induction_rna)
-write.csv(combined_results, file = "./results/genomics/top_features_results_inducion_RNA_signature.csv", row.names = FALSE)
+write.csv(combined_results, file = "/Volumes/Shared/Noah Greenwald/TONIC_Cohort/sequencing_data/prediction/top_features_results_inducion_RNA_signature.csv", row.names = FALSE)
 
-######################### ############################# 
+######################### #############################
 ######################### On-nivo ####################
 ######################### RNA #########################
 ######################### #############################
 
-df_X_matrix = read.csv('data/genomics/rna_df_matrix_on_nivo_signature.csv', header=TRUE)
+df_X_matrix = read.csv('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/sequencing_data/prediction/multivariate_lasso/rna_df_matrix_on_nivo_signature.csv', header=TRUE)
 colnames(df_X_matrix)[1] = "Patient_ID"
 
-df = read.csv('data/genomics/processed_genomics_features_final.csv', header=TRUE)
+df = read.csv('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/sequencing_data/prediction/processed_genomics_features.csv', header=TRUE)
 df_label = df[,c("Patient_ID", "Clinical_benefit")]
 aggregated_df = df_label %>%
   distinct()
@@ -160,33 +159,33 @@ i = 1
 for (seed_i in seed_seq){
   set.seed(seed_i)
   folds =  stratified_k_fold_indices(y = response_label_nivo_rna, k = nfolds)
-  
-  fit_baseline = cv.glmnet(feature_nivo_rna, #feature_baseline, 
-                           response_label_nivo_rna, 
-                           family='binomial', 
-                           type.measure='auc', 
+
+  fit_baseline = cv.glmnet(feature_nivo_rna, #feature_baseline,
+                           response_label_nivo_rna,
+                           family='binomial',
+                           type.measure='auc',
                            foldid = folds,
                            standardize=FALSE)
-  
-  auc_baseline = max(fit_baseline$cvm) 
+
+  auc_baseline = max(fit_baseline$cvm)
   auc_baseline_list_rna_nivo = c(auc_baseline_list_rna_nivo, auc_baseline)
-  
+
   feature_selected_nivo_rna[[i]] = get_ranked_feature(fit_baseline, feature_nivo_rna)[1:20,]
   i = i + 1
 }
 
 combined_results <- do.call(cbind, feature_selected_nivo_rna)
-write.csv(combined_results, file = "./results/genomics//top_features_results_nivo_RNA_signature.csv", row.names = FALSE)
+write.csv(combined_results, file = "/Volumes/Shared/Noah Greenwald/TONIC_Cohort/sequencing_data/prediction/top_features_results_nivo_RNA_signature.csv", row.names = FALSE)
 
-######################### ############################# 
+######################### #############################
 ######################### Baseline ####################
 ######################### DNA #########################
 ######################### #############################
 
-df_X_matrix = read.csv('data/genomics/dna_df_matrix_baseline.csv', header=TRUE)
+df_X_matrix = read.csv('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/sequencing_data/prediction/multivariate_lasso/dna_df_matrix_baseline.csv', header=TRUE)
 colnames(df_X_matrix)[1] = "Patient_ID"
 
-df = read.csv('data/genomics/processed_genomics_features_final.csv', header=TRUE)
+df = read.csv('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/sequencing_data/prediction/processed_genomics_features.csv', header=TRUE)
 df_label = df[,c("Patient_ID", "Clinical_benefit")]
 aggregated_df = df_label %>%
   distinct()
@@ -212,22 +211,22 @@ auc_baseline_list_dna = c()
 for (seed_i in seed_seq){
   set.seed(seed_i)
   folds =  stratified_k_fold_indices(y = response_label_baseline_rna, k = nfolds)
-  
-  fit_baseline = cv.glmnet(feature_baseline_rna, #feature_baseline, 
-                           response_label_baseline_rna, 
-                           family='binomial', 
-                           type.measure='auc', 
+
+  fit_baseline = cv.glmnet(feature_baseline_rna, #feature_baseline,
+                           response_label_baseline_rna,
+                           family='binomial',
+                           type.measure='auc',
                            foldid = folds,
                            standardize=FALSE)
-  
-  auc_baseline = max(fit_baseline$cvm) 
+
+  auc_baseline = max(fit_baseline$cvm)
   auc_baseline_list_dna = c(auc_baseline_list_dna, auc_baseline)
 }
 
 # save all results in csv
 all_results = cbind(auc_baseline_list_rna, auc_baseline_list_rna_induction, auc_baseline_list_rna_nivo, auc_baseline_list_dna)
 colnames(all_results) = c("RNA Baseline", "RNA Induction", "RNA Nivo", "DNA Baseline")
-write.csv(all_results, file = "./results/genomics/all_timepoints_results_genomics.csv", row.names = FALSE)
+write.csv(all_results, file = "/Volumes/Shared/Noah Greenwald/TONIC_Cohort/sequencing_data/prediction/multivariate_lasso/all_timepoints_results_genomics.csv", row.names = FALSE)
 
 
 
